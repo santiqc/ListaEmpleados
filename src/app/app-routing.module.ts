ee484466ad02769e4db546f22c6a03dac40b4bba
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2023-01-08 20:54:48 -0500

    Creacion de vistas y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmpleadosComponent } from './components/empleados/create-empleados/create-empleados.component';
+import { ListEmpleadosComponent } from './components/empleados/list-empleados/list-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListEmpleadosComponent},
+  {path: 'empleados', component: ListEmpleadosComponent},
+  {path: 'createEmpleado', component: CreateEmpleadosComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { ListEmpleadosComponent } from './components/empleados/list-empleados/list-empleados.component';
+import { CreateEmpleadosComponent } from './components/empleados/create-empleados/create-empleados.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListEmpleadosComponent,
+    CreateEmpleadosComponent
   ],
   imports: [
     BrowserModule,
